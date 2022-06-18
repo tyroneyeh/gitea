c7ae9bf4b9b735a525389b1af78468bbc013e7c1
Tyrone Yeh <tyrone_yeh@draytek.com>
2022-06-18 09:04:09 +0800

    Fix #5 for next page issue again.

--- routers/web/repo/issue.go
+++ routers/web/repo/issue.go
@@ -354,7 +354,8 @@ func issues(ctx *context.Context, milestoneID, projectID int64, isPullOption uti
 	ctx.Data["ViewType"] = viewType
 	ctx.Data["SortType"] = sortType
 	ctx.Data["MilestoneID"] = milestoneID
-	ctx.Data["AssigneeID"] = assigneeIDs
+	ctx.Data["AssigneeIDs"] = assigneeIDs
+	ctx.Data["AssigneeID"] = assigneeID
 	ctx.Data["IsShowClosed"] = isShowClosed
 	ctx.Data["Keyword"] = keyword
 	if isShowClosed {
